161786f34d3183079b0a0b1e9f29dcf66a51b967
NITIN Pandit <nitpandit96@gmail.com>
2024-12-14 13:45:52 +0530

    changes made into EmployeeController class added logger

--- src/main/java/com/nsp/controller/EmployeeController.java
+++ src/main/java/com/nsp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.nsp.controller;
 import com.nsp.model.Employee;
 import com.nsp.service.IEmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeController {
 
     private final IEmployeeService iEmployeeService;
 
     @PostMapping("/signUp")
     public ResponseEntity<String> signUp(@RequestBody Employee employee){
+        log.info("Trying to save employee into db");
         iEmployeeService.signUp(employee);
         return ResponseEntity.ok("Sign Up Successfullys");
     }
